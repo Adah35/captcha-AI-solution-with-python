c59b46d242d64915abf4574584de430dab7dd897
joshmatt <joshuaadah6@gmail.com>
2023-07-31 14:27:04 +0100

    new

--- main.py
+++ main.py
@@ -7,14 +7,17 @@ screen_box = (700, 300, 1400, 900)
 
 def read_screen(vartices):
     screen = ImageGrab.grab(bbox=vartices)
+    print(screen)
     return screen
         
-def display_screen(image_array):
-    image = cv2.cvtColor(np.array(image_array), cv2.COLOR_BGR2GRAY)
+def display_screen():
     while True:
+        image_array = read_screen(screen_box)   
+        image = cv2.cvtColor(np.array(image_array), cv2.COLOR_BGR2GRAY)
         cv2.imshow('screen',image)
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
+            break
 
 def read_text_from_screen(screen):
     read_text = ptr.image_to_string(screen)
@@ -31,6 +34,5 @@ def recognize_object():
 def input_keys():
     pass
 
-if '__name__' == '__main__':
-    screen = read_screen(screen_box)
-    display_screen(screen)
+if __name__ == '__main__':
+    display_screen()
